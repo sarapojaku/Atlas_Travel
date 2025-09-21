34fcd04f7361452a855a03df776d20e9d65824bb
Sara Pojaku <115804147+sarapojaku@users.noreply.github.com>
2025-09-21 18:16:10 +0200

    Create allDest.php

---
+++ allDest.php
@@ -0,0 +1,27 @@
+<?php 
+include 'db_connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Destinations</title>
+    <style>
+
+    </style>
+</head>
+<body>
+    <?php include 'header.php';?>
+
+
+    <!-- Contact Us Section -->
+    <?php include 'contact.php';?>
+
+    <!-- Footer Section -->
+    <?php include 'footer.php';?>
+
+    
+</body>
+</html>
