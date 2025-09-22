c09762cd663e1747df96b2de45fafc78ebcc96df
Sara Pojaku <115804147+sarapojaku@users.noreply.github.com>
2025-09-22 20:15:27 +0200

    Update allDest.php

--- allDest.php
+++ allDest.php
@@ -71,6 +71,8 @@ $imagePath = "uploads/";
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>All Destinations</title>
+  <link rel="icon" href="images/logo.png" type="image/png" />
+  <link rel="shortcut icon" href="images/logo.png" type="image/png" />
 <style>
     body {
         margin: 0;
