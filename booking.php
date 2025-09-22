91a28dcb1d756719729df5d2f977bdd4a00230e7
Sara Pojaku <115804147+sarapojaku@users.noreply.github.com>
2025-09-22 22:26:00 +0200

    Update booking.php

---
+++ booking.php
@@ -0,0 +1,19 @@
+<?php 
+session_start();
+include 'db_connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booking</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
